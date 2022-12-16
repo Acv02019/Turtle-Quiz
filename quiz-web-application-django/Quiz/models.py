c12158eb0620fed76674c963afdcafeab9d6a34c
Acv02019 <aaronvarner3@gmail.com>
2022-12-16 16:18:50 -0600

    Made the app (first draft)

---
+++ quiz-web-application-django/Quiz/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+class QuesModel(models.Model):
+    question = models.CharField(max_length=200,null=True)
+    op1 = models.CharField(max_length=200,null=True, blank=True)
+    op2 = models.CharField(max_length=200,null=True, blank=True)
+    ans = models.CharField(max_length=200,null=True)
+
+    def __str__(self):
+        return self.question
